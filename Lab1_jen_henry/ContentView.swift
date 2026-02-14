416dda44f75d40be64cf23d32de0e456785eda95
Jen Henry <jokodzena@gmail.com>
2026-02-14 13:15:46 -0500

    Added private variable totalAttemps

--- Lab1_jen_henry/ContentView.swift
+++ Lab1_jen_henry/ContentView.swift
@@ -4,6 +4,7 @@ struct ContentView: View {
     @State private var currentNumber = Int.random(in: 2...100)
     @State private var correctCount = 0
     @State private var wrongCount = 0
+    @State private var totalAttempts = 0
 }
 
 #Preview {
