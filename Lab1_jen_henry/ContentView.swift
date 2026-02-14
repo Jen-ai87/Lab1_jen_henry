cba57237862970a864ca1c008a8a71b3baa743ad
Jen Henry <jokodzena@gmail.com>
2026-02-14 13:15:06 -0500

    Added private variable wrongCount

--- Lab1_jen_henry/ContentView.swift
+++ Lab1_jen_henry/ContentView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var currentNumber = Int.random(in: 2...100)
     @State private var correctCount = 0
+    @State private var wrongCount = 0
 }
 
 #Preview {
